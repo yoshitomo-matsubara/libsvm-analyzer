8019d66527df6d3477e5ed6597facfa5f3afce44
aa14h507 <aa14h507@ai.u-hyogo.ac.jp>
2016-04-25 22:50:22 +0900

    added Constant class

---
+++ src/common/Constant.java
@@ -0,0 +1,13 @@
+package common;
+
+public class Constant
+{
+    public static final String SPACE_DELIMITER = " ";
+    public static final String TAB_DELIMITER = "\t";
+    public static final String COMMA_DELIMITER = ",";
+    public static final String KEY_VALUE_DELIMITER = ":";
+    public static final String TARGET_LABEL = "1";
+    public static final String NON_TARGET_LABEL = "-1";
+    public static final int TARGET_VALUE = 1;
+    public static final int NON_TARGET_VALUE = -1;
+}
